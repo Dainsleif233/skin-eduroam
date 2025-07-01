3536ecaebcd2d2129af044a24993d25212799f76
Dainsleif <dainsleif@yeah.net>
2025-07-01 22:59:01 +0800

    提供备用认证

--- src/AuthController.php
+++ src/AuthController.php
@@ -56,55 +56,140 @@ class AuthController extends Controller {
         $ip = $filter->apply('client_ip', $ip);
         if (User::where('ip', $ip)->count() >= option('regs_per_ip')) return json(trans('auth.register.max', ['regs' => option('regs_per_ip')]), 1);
         $username = $data['user'];
-        $eduroam_user = option('eduroam_domain', null) ? $username . '@' . option('eduroam_domain') : $username;
-        if (User::where('eduroam', $eduroam_user)->first()) return json(trans('Blessing\\Eduroam::eduroam.auth.user_repeated'), 1);
+
+        $auth_result = $this->auth($username, $data['password']);
+        if ($auth_result['code'] == 0)
+            return $this->register($dispatcher, $data, $ip, $auth_result['eduroam_user'], $playerName, $request, $filter);
+        elseif ($auth_result['code'] == 1 || !option('backup_eduroam_api'))
+            return json($auth_result['message'], 1);
+
+        $auth_result = $this->auth_backup($username, $data['password']);
+        if ($auth_result['code'] == 0)
+            return $this->register($dispatcher, $data, $ip, $auth_result['eduroam_user'], $playerName, $request, $filter);
+        return json($auth_result['message'], 1);
+    }
+
+    private function auth($username, $password) {
+        $login = option('eduroam_domain', null) ? $username . '@' . option('eduroam_domain') : $username;
+        if (User::where('eduroam', $login)->first())
+            return [
+                'eduroam_user' => $login,
+                'message' => trans('Blessing\\Eduroam::eduroam.auth.user_repeated'),
+                'code' => 1
+            ];
         try {
             $response = Http::asForm()->post(option('eduroam_api', 'https://eduroam.ustc.edu.cn/cgi-bin/eduroam-test.cgi'), [
-                'login' => $eduroam_user,
-                'password' => $data['password']
+                'login' => $login,
+                'password' => $password
             ]);
         } catch (Exception $e) {
             Log::error('Eduroam API failed: '.$e->getMessage());
-            return json(trans('Blessing\\Eduroam::eduroam.auth.api_failure'), 1);
+            return [
+                'eduroam_user' => $login,
+                'message' => trans('Blessing\\Eduroam::eduroam.auth.api_failure'),
+                'code' => 3
+            ];
         }
-        if (strpos($response->body(), 'EAP Failure')) return json(trans('Blessing\\Eduroam::eduroam.auth.failure'), 1);
-        elseif (strpos($response->body(), 'illegal')) return json(trans('Blessing\\Eduroam::eduroam.auth.illegal'), 1);
-        elseif (strpos($response->body(), 'EAP Success')) {
-            $dispatcher->dispatch('auth.registration.ready', [$data]);
-            $user = new User();
-            $user->email = $data['qq'] . '@qq.com';
-            $user->nickname = $data[option('register_with_player_name') ? 'player_name' : 'nickname'];
-            $user->score = option('user_initial_score');
-            $user->avatar = 0;
-            $password = app('cipher')->hash($data['password'], config('secure.salt'));
-            $user->password = $filter->apply('user_password', $password);
-            $user->ip = $ip;
-            $user->permission = User::NORMAL;
-            $user->register_at = Carbon::now();
-            $user->last_sign_at = Carbon::now()->subDay();
-            $user->eduroam = $eduroam_user;
-            $user->save();
-            $eduroam = Eduroam::firstOrCreate(
-                ['eduroam' => $eduroam_user],
-                ['name' => [], 'qq' => []]
-            );
-            $eduroam->addQQ($data['qq'])->save();
-            $dispatcher->dispatch('auth.registration.completed', [$user]);
-            event(new Events\UserRegistered($user));
-            if (option('register_with_player_name')) {
-                $dispatcher->dispatch('player.adding', [$playerName, $user]);
-                $player = new Player();
-                $player->uid = $user->uid;
-                $player->name = $playerName;
-                $player->tid_skin = 0;
-                $player->save();
-                $dispatcher->dispatch('player.added', [$player, $user]);
-                event(new Events\PlayerWasAdded($player));
-            }
-            $dispatcher->dispatch('auth.login.ready', [$user]);
-            auth()->login($user);
-            $dispatcher->dispatch('auth.login.succeeded', [$user]);
-            return json(trans('auth.login.success'), 0, ['redirectTo' => $request->session()->pull('last_requested_path', url('/user'))]);
-        } else return json(trans('Blessing\\Eduroam::eduroam.auth.unknown'), 1);
+        if (strpos($response->body(), 'EAP Failure'))
+            return [
+                'eduroam_user' => $login,
+                'message' => trans('Blessing\\Eduroam::eduroam.auth.failure'),
+                'code' => 1
+            ];
+        elseif (strpos($response->body(), 'illegal'))
+            return [
+                'eduroam_user' => $login,
+                'message' => trans('Blessing\\Eduroam::eduroam.auth.illegal'),
+                'code' => 2
+            ];
+        elseif (strpos($response->body(), 'EAP Success'))
+            return [
+                'eduroam_user' => $login,
+                'code' => 0
+            ];
+        else
+            return [
+                'eduroam_user' => $login,
+                'message' => trans('Blessing\\Eduroam::eduroam.auth.unknown'),
+                'code' => 3
+            ];
+    }
+
+    private function auth_backup($username, $password) {
+        $login = option('backup_eduroam_domain', null) ? $username . '@' . option('backup_eduroam_domain') : $username;
+        try {
+            $response = Http::asForm()->post(option('backup_eduroam_api'), [
+                'login' => $login,
+                'password' => $password
+            ]);
+        } catch (Exception $e) {
+            Log::error('Backup Eduroam API failed: '.$e->getMessage());
+            return [
+                'eduroam_user' => $login,
+                'message' => trans('Blessing\\Eduroam::eduroam.auth.api_failure'),
+                'code' => 3
+            ];
+        }
+        if (strpos($response->body(), 'EAP Failure'))
+            return [
+                'eduroam_user' => $login,
+                'message' => trans('Blessing\\Eduroam::eduroam.auth.failure'),
+                'code' => 1
+            ];
+        elseif (strpos($response->body(), 'illegal'))
+            return [
+                'eduroam_user' => $login,
+                'message' => trans('Blessing\\Eduroam::eduroam.auth.illegal'),
+                'code' => 2
+            ];
+        elseif (strpos($response->body(), 'EAP Success'))
+            return [
+                'eduroam_user' => $login,
+                'code' => 0
+            ];
+        else
+            return [
+                'eduroam_user' => $login,
+                'message' => trans('Blessing\\Eduroam::eduroam.auth.unknown'),
+                'code' => 3
+            ];
+    }
+
+    private function register($dispatcher, $data, $ip, $eduroam_user, $playerName, $request, $filter) {
+        $dispatcher->dispatch('auth.registration.ready', [$data]);
+        $user = new User();
+        $user->email = $data['qq'] . '@qq.com';
+        $user->nickname = $data[option('register_with_player_name') ? 'player_name' : 'nickname'];
+        $user->score = option('user_initial_score');
+        $user->avatar = 0;
+        $password = app('cipher')->hash($data['password'], config('secure.salt'));
+        $user->password = $filter->apply('user_password', $password);
+        $user->ip = $ip;
+        $user->permission = User::NORMAL;
+        $user->register_at = Carbon::now();
+        $user->last_sign_at = Carbon::now()->subDay();
+        $user->eduroam = $eduroam_user;
+        $user->save();
+        $eduroam = Eduroam::firstOrCreate(
+            ['eduroam' => $eduroam_user],
+            ['name' => [], 'qq' => []]
+        );
+        $eduroam->addQQ($data['qq'])->save();
+        $dispatcher->dispatch('auth.registration.completed', [$user]);
+        event(new Events\UserRegistered($user));
+        if (option('register_with_player_name')) {
+            $dispatcher->dispatch('player.adding', [$playerName, $user]);
+            $player = new Player();
+            $player->uid = $user->uid;
+            $player->name = $playerName;
+            $player->tid_skin = 0;
+            $player->save();
+            $dispatcher->dispatch('player.added', [$player, $user]);
+            event(new Events\PlayerWasAdded($player));
+        }
+        $dispatcher->dispatch('auth.login.ready', [$user]);
+        auth()->login($user);
+        $dispatcher->dispatch('auth.login.succeeded', [$user]);
+        return json(trans('auth.login.success'), 0, ['redirectTo' => $request->session()->pull('last_requested_path', url('/user'))]);
     }
 }
--- src/ConfigController.php
+++ src/ConfigController.php
@@ -9,9 +9,11 @@ use Option;
 class ConfigController extends Controller {
     public function render() {
         $configForm = Option::form('config', trans('Blessing\\Eduroam::eduroam.config.config'), function (OptionForm $form) {
-            $form->checkbox('replace', trans('Blessing\\Eduroam::eduroam.config.replace'))->label(trans('Blessing\\Eduroam::eduroam.config.replace-label'))->value(true)->disabled();
             $form->text('eduroam_domain', trans('Blessing\\Eduroam::eduroam.config.eduroam_domain'))->hint(trans('Blessing\\Eduroam::eduroam.config.eduroam_domain-hint'));
             $form->text('eduroam_api', trans('Blessing\\Eduroam::eduroam.config.eduroam_api'))->description(trans('Blessing\\Eduroam::eduroam.config.eduroam_api-description'));
+            $form->text('backup_eduroam_domain', trans('Blessing\\Eduroam::eduroam.config.backup_eduroam_domain'))->hint(trans('Blessing\\Eduroam::eduroam.config.eduroam_domain-hint'));
+            $form->text('backup_eduroam_api', trans('Blessing\\Eduroam::eduroam.config.backup_eduroam_api'))->description(trans('Blessing\\Eduroam::eduroam.config.backup_eduroam_api-description'));
+            $form->checkbox('replace', trans('Blessing\\Eduroam::eduroam.config.replace'))->label(trans('Blessing\\Eduroam::eduroam.config.replace-label'))->value(true)->disabled();
             $form->checkbox('prevent_edit_email', trans('Blessing\\Eduroam::eduroam.config.prevent_edit_email'))->label(trans('Blessing\\Eduroam::eduroam.config.prevent_edit_email-label'))->value(false)->disabled();
         })->handle();
         return view('Blessing\Eduroam::config', ['config' => $configForm]);
